a2e2037cdbc3455347a6bdc14d75e535627d3f0a
GoldGatherer <GoldGatherer.ru@mail.ru>
2020-08-30 17:26:17 +0300

    Updates

---
+++ precache-manifest.b8e4fae0a623979cbb993238509387e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9f7cc4a8b33c9e25f6bd0684f5ba8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9269530948115b3f28e4",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "6a88d5e51c2a51dfda80",
+    "url": "./static/css/main.d9c443e2.chunk.css"
+  },
+  {
+    "revision": "9269530948115b3f28e4",
+    "url": "./static/js/2.6b765a5b.chunk.js"
+  },
+  {
+    "revision": "a0e5d688115e07ffc9e00c6865506a4a",
+    "url": "./static/js/2.6b765a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a88d5e51c2a51dfda80",
+    "url": "./static/js/main.d322c0fa.chunk.js"
+  },
+  {
+    "revision": "7c60a33eab18d047b6c1",
+    "url": "./static/js/runtime-main.52b2cf8c.js"
+  },
+  {
+    "revision": "6b117ee703817520e175233f6d12a831",
+    "url": "./static/media/test1.6b117ee7.jpg"
+  },
+  {
+    "revision": "0af729482909efd74befed1b04e55922",
+    "url": "./static/media/test2.0af72948.jpg"
+  }
+]);
